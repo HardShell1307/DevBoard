6395c61bfee262107e7bd8bb063340d79d678838
Jiyavur <jiyavurrahman80@gmail.com>
2026-07-31 12:41:13 +0530

    Add custom column creation with localStorage

--- devboard/client/src/components/Board/KanbanBoard.jsx
+++ devboard/client/src/components/Board/KanbanBoard.jsx
@@ -3,7 +3,7 @@ import { DragDropContext } from "@hello-pangea/dnd";
 import Column from "./Column";
 import TaskModal from "../Task/TaskModal";
 import { useBoard } from "../../context/BoardContext";
-import confetti from 'canvas-confetti';
+import confetti from "canvas-confetti";
 
 const COLUMNS = ["backlog", "inprogress", "review", "done"];
 
@@ -12,28 +12,30 @@ const KanbanBoard = ({ onSelectTask }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [defaultStatus, setDefaultStatus] = useState("backlog");
 
+  const [columns, setColumns] = useState(() => {
+      const saved = localStorage.getItem("columns");
+      return saved ? JSON.parse(saved) : COLUMNS;
+});
+  const [showinput, setShowinput] = useState(false);
+  const [columnName, setColumnName] = useState("");
+
   useEffect(() => {
-  const handler = (e) => {
-    if (
-      e.key.toLowerCase() === "n" &&
-      !e.target.matches("input, textarea")
-    ) {
-      setDefaultStatus("backlog");
-      setModalOpen(true);
-    }
-  };
+    const handler = (e) => {
+      if (e.key.toLowerCase() === "n" && !e.target.matches("input, textarea")) {
+        setDefaultStatus("backlog");
+        setModalOpen(true);
+      }
+    };
 
-  window.addEventListener("keydown", handler);
+    window.addEventListener("keydown", handler);
 
-  return () => {
-    window.removeEventListener("keydown", handler);
-  };
-}, []);
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, []);
 
   const getTasksByStatus = (status) =>
-    tasks
-      .filter((t) => t.status === status)
-      .sort((a, b) => a.order - b.order);
+    tasks.filter((t) => t.status === status).sort((a, b) => a.order - b.order);
 
   const onDragEnd = async (result) => {
     const { destination, source } = result;
@@ -41,7 +43,7 @@ const KanbanBoard = ({ onSelectTask }) => {
     if (!destination) return;
 
     if (destination.droppableId === "done") {
-    confetti({ particleCount: 100, spread: 70 });
+      confetti({ particleCount: 100, spread: 70 });
     }
 
     const colTasks = getTasksByStatus(destination.droppableId);
@@ -55,8 +57,8 @@ const KanbanBoard = ({ onSelectTask }) => {
         updateTask(task._id, {
           status: destination.droppableId,
           order: index,
-        })
-      )
+        }),
+      ),
     );
   };
 
@@ -65,11 +67,58 @@ const KanbanBoard = ({ onSelectTask }) => {
     setModalOpen(true);
   };
 
+  const handleSubmit = () => {
+
+    if (!columnName.trim()) {
+      console.log("Empty column name!");
+      return;
+    }
+
+    setColumns((prev) => {
+      const updated = [...prev, columnName];
+      return updated;
+    });
+
+    setColumnName("");
+    setShowinput(false);
+  };
+
+  const handleCancel = () => {
+    setShowinput(false);
+    setColumnName("");
+  };
+
+  useEffect(() => {
+  localStorage.setItem("columns", JSON.stringify(columns));
+   }, [columns]);
+
   return (
     <>
+      {showinput && (
+        <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center">
+          <div className="flex flex-col bg-white rounded-lg p-6 w-96 shadow-xl">
+            <input
+              type="text"
+              placeholder="Enter column name"
+              value={columnName}
+              onChange={(e) => setColumnName(e.target.value)}
+              className="border rounded px-2 py-1 placeholder-gray-300 text-gray-950"
+            />
+            <div className="flex justify-between">
+              <button onClick={handleCancel} className="p-4 text-red-600">
+                Cancel
+              </button>
+              <button onClick={handleSubmit} className="p-4 text-green-600">
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <DragDropContext onDragEnd={onDragEnd}>
         <div className="flex flex-col md:flex-row gap-4 p-4 overflow-x-hidden md:overflow-x-auto overflow-y-auto h-full">
-          {COLUMNS.map((col) => (
+          {columns.map((col) => (
             <Column
               key={col}
               columnId={col}
@@ -78,6 +127,14 @@ const KanbanBoard = ({ onSelectTask }) => {
               onAddTask={handleAddTask}
             />
           ))}
+          <button
+            className="flex"
+            onClick={() => {
+              setShowinput(true);
+            }}
+          >
+            + Add Column
+          </button>
         </div>
       </DragDropContext>
 
